356c740273a69ad2e0de885b7ee6efac52b48188
widi-ps <widyapujisaputro@gmail.com>
2022-05-25 22:07:20 +0700

    Firebase Realtime Database

--- app/src/main/java/com/example/firebaseauth/MainActivity.kt
+++ app/src/main/java/com/example/firebaseauth/MainActivity.kt
@@ -5,15 +5,23 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.firebaseauth.databinding.ActivityMainBinding
+import com.firebase.ui.database.FirebaseRecyclerOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
 	private lateinit var binding: ActivityMainBinding
 	private lateinit var auth: FirebaseAuth
+	private lateinit var db: FirebaseDatabase
+	private lateinit var adapter: FirebaseMessageAdapter
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
@@ -29,6 +37,47 @@ class MainActivity : AppCompatActivity() {
 			finish()
 			return
 		}
+
+		db = Firebase.database
+
+		val messageRef = db.reference.child(MESSAGES_CHILD)
+
+		binding.sendButton.setOnClickListener {
+			val friendlyMessage = Message(
+				binding.messageEditText.text.toString(),
+				firebaseUser.displayName.toString(),
+				firebaseUser.photoUrl.toString(),
+				Date().time
+			)
+			messageRef.push().setValue(friendlyMessage) { error, _ ->
+				if(error != null){
+					Toast.makeText(this,getString(R.string.send_error) + error.message, Toast.LENGTH_SHORT).show()
+				} else {
+					Toast.makeText(this, getString(R.string.send_success), Toast.LENGTH_SHORT).show()
+				}
+			}
+			binding.messageEditText.setText("")
+		}
+
+		val manager = LinearLayoutManager(this)
+		manager.stackFromEnd = true
+		binding.messageRecyclerView.layoutManager = manager
+
+		val options = FirebaseRecyclerOptions.Builder<Message>()
+			.setQuery(messageRef, Message::class.java)
+			.build()
+		adapter = FirebaseMessageAdapter(options, firebaseUser.displayName)
+		binding.messageRecyclerView.adapter = adapter
+	}
+
+	public override fun onResume() {
+		super.onResume()
+		adapter.startListening()
+	}
+
+	public override fun onPause() {
+		adapter.stopListening()
+		super.onPause()
 	}
 
 	override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -51,5 +100,9 @@ class MainActivity : AppCompatActivity() {
 		auth.signOut()
 		startActivity(Intent(this, LoginActivity::class.java))
 		finish()
+	}
+
+	companion object {
+		const val MESSAGES_CHILD = "messages"
 	}
 }
---
+++ app/src/main/java/com/example/firebaseauth/Message.kt
@@ -0,0 +1,11 @@
+package com.example.firebaseauth
+
+data class Message(
+	val text: String? = null,
+	val name: String? = null,
+	val photoUrl: String? = null,
+	val timestamp: Long? = null
+){
+
+}
+
